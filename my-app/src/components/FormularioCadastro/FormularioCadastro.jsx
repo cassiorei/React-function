e30cd79c07d54c5419e230008513ce3f3c832b6c
cassio <cassiorei98@gmail.com>
2022-01-21 15:56:35 -0300

    criando estilo em react

--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,32 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { TextField } from "@material-ui/core";
 
-function FormularioCadastro (){
-      return (
-      <form>
-           <label>Nome</label>
-          <input type="text"/>
+function FormularioCadastro() {
+  return (
+    <form>
+      <label>Nome</label>
+      <input type="text" />
 
-          <label>Sobrenome</label>
-          <input type="text" />
+      <label>Sobrenome</label>
+      <input type="text" />
 
-          <label>CPF</label>
-          <input type="text"/>
+      <label>CPF</label>
+      <input type="text" />
 
-          <label>Promocões</label>
-          <input type="checkbox" />
+      <label>Promoções</label>
+      <input type="checkbox" />
+      <label>Novidades</label>
+      <input type="checkbox" />
 
-          <label>Novidades</label>
-          <input type="checkbox"  />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
 
-          <button type="submit">Cadastrar</button>
-      </form>);
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormularioCadastro;
